202808a687cc4234dd1e95d96d0cef9d05232e71
BryanEdou456 <bedou9121@floridapoly.edu>
2025-11-12 17:06:38 +0000

    Rand choice wtv

--- game.py
+++ game.py
@@ -1,3 +1,7 @@
-print("Welcome to Rock Paper Scissors!")
-choice = input("Choose rock, paper, or scissors: ")
-print("You chose: " + choice)
+import random
+print("Welcome to Rock-Paper-Scissors!")
+
+
+RPS = ["Rock", "Paper", "Scissors"]
+ComChoice = random.choice(RPS)
+print(f"Computer chose: {ComChoice}")
